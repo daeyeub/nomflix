cda83fdbe3af46b8421a5950c3ecf7458b0d00b8
daeyeub <97010621+daeyeub@users.noreply.github.com>
2022-08-27 20:09:43 +0900

    2022-08-27

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Search from "./Routes/Search";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Header from "./Components/Header";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tv" element={<Tv />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
