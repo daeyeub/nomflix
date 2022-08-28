6fef4e799ca42bc02b6b72ad064ce6f526bb71c9
daeyeub <97010621+daeyeub@users.noreply.github.com>
2022-08-28 19:01:43 +0900

    2022-08-28

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,23 @@
 const API_KEY = "e2f19e7a53926bd1e00596b678b03d80";
 const BASE_URL = "https://api.themoviedb.org/3";
+
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
 export function getMovies() {
   return fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`).then(
     (response) => response.json()
